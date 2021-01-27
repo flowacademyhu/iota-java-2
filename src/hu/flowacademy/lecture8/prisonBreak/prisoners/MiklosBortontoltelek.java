9f6b64a24b5e5835f45d38a2b1298c8250cb18e9
gyanyim <gyanyi.miklos@gmail.com>
2021-01-27 11:00:36 +0100

    Miklos borton feladat

---
+++ src/hu/flowacademy/lecture8/prisonBreak/prisoners/MiklosBortontoltelek.java
@@ -0,0 +1,22 @@
+package prisonBreak.prisoners;
+
+import hu.flowacademy.lecture8.prisonBreak.Borton;
+import hu.flowacademy.lecture8.prisonBreak.Bortontoltelek;
+
+public class MiklosBortontoltelek extends Bortontoltelek {
+
+    private boolean kapcsoltamEMarFel = false;
+    @Override
+    public String neve() {
+        return "Miklos";
+    }
+    @Override
+    public boolean voltKintMindenki() {
+        if (!Borton.lampaVilagit() && !kapcsoltamEMarFel) {
+            Borton.lampaFelkapcsol();
+            kapcsoltamEMarFel = true;
+        }
+        return false;
+    }
+
+}
